04cb75f37c115e854e2dfd78341d8739c1af7015
mysilence1408 <mysilence1408@gmail.com>
2025-02-15 22:28:01 +0330

    first commit

--- src/components/Home/TabFeatures.tsx
+++ src/components/Home/TabFeatures.tsx
@@ -97,7 +97,7 @@ const Tabs = ({ selected, setSelected }: TabsProps) => {
     }
   }, [selected]);
   return (
-    <div className="flex overflow-x-auto scrollbar-hide justify-center">
+    <div className="flex overflow-x-auto scrollbar-hide lg:justify-center">
       {FEATURES.map((tab, index) => (
         <div
           key={index}
