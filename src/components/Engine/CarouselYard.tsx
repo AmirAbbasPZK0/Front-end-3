628959062e530d46868bec1163940f4f449abb7c
AmirAbbasPZK0 <amirabbaspezeshki2@gmail.com>
2025-03-13 16:42:03 +0330

    D

--- src/components/Engine/CarouselYard.tsx
+++ src/components/Engine/CarouselYard.tsx
@@ -1,40 +1,75 @@
 "use client"
 
-import React, { useEffect } from "react";
+import React from "react";
 import YouTubeVideos from "./YouTubeVideos";
-import useAgent from "@/hooks/useAgent";
-// import ChosenIcon from "./ChosenIcon";
-import { useState } from "react";
-import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import Carousel from "react-multi-carousel";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/free-mode";
+
+import { FreeMode, Pagination } from "swiper/modules";
+
+import { RxArrowTopRight } from "react-icons/rx";
 
 const CarouselYard = ({videos} : {videos : any}) => {
 
-    const responsive = {
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 4,
-          slidesToSlide: 1 // optional, default to 1.
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2,
-          slidesToSlide: 1 // optional, default to 1.
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1,
-          slidesToSlide: 1 // optional, default to 1.
-        }
-      };
+    // const responsive = {
+    //     desktop: {
+    //       breakpoint: { max: 3000, min: 1024 },
+    //       items: 4,
+    //       slidesToSlide: 1 // optional, default to 1.
+    //     },
+    //     tablet: {
+    //       breakpoint: { max: 1024, min: 464 },
+    //       items: 2,
+    //       slidesToSlide: 1 // optional, default to 1.
+    //     },
+    //     mobile: {
+    //       breakpoint: { max: 464, min: 0 },
+    //       items: 1,
+    //       slidesToSlide: 1 // optional, default to 1.
+    //     }
+    //   };
 
     return(<>
-        {videos?.length > 0 && (<>
-          <Carousel responsive={responsive}>
+        {/* {videos?.length > 0 && (<>
+          <Carousel arrows={false} responsive={responsive}>
             {videos?.map((item : any , index : number) => (
                 <YouTubeVideos key={index} url={item.link} data={item}/>
             ))}
           </Carousel>
+        </>)} */}
+        {videos?.length > 0 && (<>
+          <Swiper
+            breakpoints={{
+              1: {
+                slidesPerView: 1,
+                spaceBetween: 15,
+              },
+              700: {
+                slidesPerView: 4,
+                spaceBetween: 15,
+              },
+            }}
+            freeMode={true}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[FreeMode, Pagination]}
+            navigation={{
+              prevEl: ".swiper-button-prev",
+              nextEl: ".swiper-button-next",
+            }}
+          >
+            {videos?.map((item : any , index : number) => (
+                <SwiperSlide key={index}>
+                  <YouTubeVideos url={item.link} data={item}/>
+                </SwiperSlide>
+            ))}
+          </Swiper>
         </>)}
     </>)
 }
