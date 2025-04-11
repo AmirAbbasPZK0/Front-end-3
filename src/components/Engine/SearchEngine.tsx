c433534ba811b498530f54fb0084226fb2efeecc
AmirAbbasPZK0 <amirabbaspezeshki2@gmail.com>
2025-04-11 19:36:03 +0330

    d

--- src/components/Engine/SearchEngine.tsx
+++ src/components/Engine/SearchEngine.tsx
@@ -10,7 +10,7 @@ const SearchEngine = () => {
     return (<>
         <div className="min-h-[80vh] w-full flex flex-col items-cneter justify-center dark:bg-[#111828]">
             {isNew && <div className="w-full flex items-center justify-center">
-                <h3 className="text-[24px] md:text-[36px] md:font-light font-normal text-zinc-900 dark:text-slate-200 antialiased text-center">Need the right info? Let’s find it.</h3>
+                <h3 className="text-[24px] md:text-[36px] md:font-light font-normal text-zinc-900 dark:text-slate-200 antialiased text-center">Ask me Anything</h3>
             </div>}
             <PropmptYard/>
         </div>
