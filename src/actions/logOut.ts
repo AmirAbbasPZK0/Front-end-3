1c8f10bb9742078e82dc4864bcd25b1d762ecd16
AmirAbbasPZK0 <amirabbaspezeshki2@gmail.com>
2025-04-21 13:59:46 +0330

    D

--- src/actions/logOut.ts
+++ src/actions/logOut.ts
@@ -2,16 +2,13 @@
 
 import { signOut } from "@/auth"
 import { cookies } from "next/headers"
-import { redirect } from "next/navigation"
 
 export async function logOut(){
 
-    // cookies().delete("authjs.callback-url")
-    // cookies().delete("authjs.csrf-token")
-    // cookies().delete("authjs.session-token")
+    (await cookies()).delete("authjs.callback-url")
+    ;(await cookies()).delete("authjs.csrf-token")
+    ;(await cookies()).delete("authjs.session-token")
     
     await signOut()
-
-    redirect("/")
 
 }
