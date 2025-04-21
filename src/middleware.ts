9a01eb818e0bf32dd2832f892f553654e2ea37a4
AmirAbbasPZK0 <amirabbaspezeshki2@gmail.com>
2025-04-21 13:21:27 +0330

    d

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,31 +8,16 @@ export async function middleware(req : NextRequest){
 
     const google_token = req.cookies.get("authjs.session-token")?.value
 
-    const token = req.cookies.get("access_token")?.value
-
     const isInPrivateRoute = privateRoutes.includes(req.nextUrl.pathname)
     const isInPublicRoute = publicRoutes.includes(req.nextUrl.pathname)
 
-    // const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/user` , {
-    //     method : "GET",
-    //     headers : {
-    //         "Authorization" : `Bearer ${token}`
-    //     }
-    // })
+    if(isInPrivateRoute && !google_token){
+        return NextResponse.redirect(new URL('/login' , req.nextUrl))
+    }
 
-    // let user : {data : any} = {data : false}
-
-    // if(res.ok){
-    //     user = await res.json()
-    // }
-
-    // if(isInPrivateRoute && (!user.data || !google_token)){
-    //     return NextResponse.redirect(new URL('/login' , req.nextUrl))
-    // }
-
-    // if(isInPublicRoute && (user.data || google_token)){
-    //     return NextResponse.redirect(new URL('/' , req.nextUrl))
-    // }
+    if(isInPublicRoute && google_token){
+        return NextResponse.redirect(new URL('/' , req.nextUrl))
+    }
 
     return NextResponse.next()
 }
