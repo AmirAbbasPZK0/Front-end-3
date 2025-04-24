951fe17eb3de4c56bda2d717aa22d5124f75fb15
AmirAbbasPZK0 <amirabbaspezeshki2@gmail.com>
2025-04-24 12:47:57 +0330

    D

--- src/components/Register/SignUpFormHandler.tsx
+++ src/components/Register/SignUpFormHandler.tsx
@@ -1,53 +1,64 @@
 "use client";
 import React, { ChangeEvent, useEffect, useRef } from "react";
 import { motion } from "framer-motion";
-import toast from "react-hot-toast";
-import Cookies from "js-cookie";
 import { useAppDispatch } from "@/services/redux/store";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { loginHandler } from "@/services/redux/reducers/userSlice";
+import restApi from "@/services/restApi";
+import endpoints from "@/configs/endpoints";
+
+interface SignUpFormProps {
+  first_name : string
+  last_name : string
+  email : string
+  password : string
+  confirm_password : string
+}
 
 
 const SignUpFormHandler: React.FC = () => {
+
+  function run(body : SignUpFormProps){
+    fetch(`http://185.110.191.217:5000/register` , {
+      method :"POST",
+      headers : {
+        "Content-Type" : "application/json"
+      },
+      body : JSON.stringify(body)
+    }).then(res => {
+      if(res.ok){
+        return res.json()
+      }
+    }).then(resData => {
+      console.log(resData)
+    }).catch(err => {
+      console.log(err)
+    })
+  }
 
   const form = useRef<HTMLFormElement | null>(null);
 
   const dispatch = useAppDispatch()
   
-    const router = useRouter()
+  const router = useRouter()
   
-    const onSubmit = async (e :  ChangeEvent<HTMLFormElement>) => {
-      e.preventDefault()
-      let data = {
-        first_name : e.target?.first_name?.value,
-        last_name : e.target?.last_name?.value,
-        confirm_password : e.target?.confirm_password?.value,
-        email : e.target?.email?.value,
-        password : e.target?.password?.value
-      }
-      let res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/register/` , {
-        method : "POST",
-        body : JSON.stringify(data),
-        headers : {
-          "Content-Type" : "application/json"
-        }
-      })
-      
-      let result = await res.json()
-  
-      if(result.code !== 200){
-        toast("Failed to Fetch")
-        return false
-      }else{
-        Cookies.set("access_token" , result?.data?.token , {path : "/" , expires : 60 * 60 * 60 * 24})
-        dispatch(loginHandler(result?.data?.user))
-        router.push("/")
-      }
-  
-      // window.location.reload()
-      
+  const onSubmit = (e :  ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    let data = {
+      first_name : e.target?.first_name?.value,
+      last_name : e.target?.last_name?.value,
+      confirm_password : e.target?.confirm_password?.value,
+      email : e.target?.email?.value,
+      password : e.target?.password?.value
     }
+
+    run(data)
+
+      // const res = await restApi(endpoints.register , false , false).post(data)
+      
+  }
 
   return (
     <div className="px-4 md:px-8 py-10 md:py-20 flex justify-center items-center">
