636c16d052245d0760f0d626ba67daf9f6cb1f75
AmirAbbasPZK0 <amirabbaspezeshki2@gmail.com>
2025-05-12 12:32:05 +0330

    D

--- src/components/Register/LoginFormHandler.tsx
+++ src/components/Register/LoginFormHandler.tsx
@@ -16,13 +16,14 @@ const LoginFormHandler: React.FC = () => {
 
   const form = useRef<HTMLFormElement | null>(null);
   const [pending , setPending] = useState(false)
+  const [loading , setLoading] = useState(false)
   const [error , setError] = useState("")
   // console.log(data)
 
   const router = useRouter()
 
   const login = async () => {
-    setPending(true)
+    // setPending(true)
     await signUp()
     await googleBackHandler()
   }
@@ -30,6 +31,7 @@ const LoginFormHandler: React.FC = () => {
   const dispatch = useAppDispatch()
 
   const onSubmit = async (e :  ChangeEvent<HTMLFormElement>) => {
+    setLoading(true)
     e.preventDefault()
     let data = {
       email : e.target?.email.value,
@@ -63,11 +65,13 @@ const LoginFormHandler: React.FC = () => {
           toast.error(error, {
             duration: 3000,
           });
+          setLoading(false)
         case 400:
           setError("Bad Request")
           toast.error(error, {
             duration: 3000,
           });
+          setLoading(false)
           break
       }
     }else{
@@ -81,6 +85,7 @@ const LoginFormHandler: React.FC = () => {
         //   color: "white",
         // },
       });
+      // setLoading(false)
       router.push("/")
     } 
   }
@@ -118,6 +123,10 @@ const LoginFormHandler: React.FC = () => {
                 required
               />
             </div>
+            <div className="flex flex-row w-full justify-between">
+              <span>Forget Password?</span>
+              <Link className="text-blue-500" href={"/reset-password"}>Reset Password</Link>
+            </div>
             <div className="text-center flex items-center justify-center">
               <h2>Or</h2>
             </div>
@@ -129,7 +138,7 @@ const LoginFormHandler: React.FC = () => {
                     <svg aria-hidden="true" className="inline w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
                       <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-                  </svg>
+                    </svg>
                   </>) : (<>
                     <FcGoogle className="h-5 w-5" />
                    <span>Continue with Google</span>    
@@ -140,7 +149,7 @@ const LoginFormHandler: React.FC = () => {
             </div>
           </div>
           <div>
-            <ShinySkeuButton />
+            <ShinySkeuButton isPending={loading}/>
           </div>
           <div className="flex flex-row items-center justify-center gap-2">
             <p>Don't have an account?</p> <Link className="text-blue-500 font-semibold" href={"/signup"}>Sign Up</Link>
@@ -171,7 +180,7 @@ const LoginFormHandler: React.FC = () => {
   );
 };
 
-const ShinySkeuButton: React.FC = () => {
+const ShinySkeuButton = ({isPending} : {isPending : boolean}) => {
   const parentRef = useRef<HTMLDivElement | null>(null);
   const btnRef = useRef<HTMLButtonElement | null>(null);
 
@@ -209,11 +218,17 @@ const ShinySkeuButton: React.FC = () => {
   return (
     <div ref={parentRef} className="skeuParent">
       <button
+        disabled={isPending}
         ref={btnRef}
         type="submit"
         className="w-full overflow-hidden font-mono cursor-pointer text-white rounded px-4 py-2 bg-[radial-gradient(100%_100%_at_100%_0%,_#af8bee_0%,_#6903f6_100%)] transition-[box-shadow_0.15s_ease,_transform_0.15s_ease] shadow-md hover:shadow-lg hover:-translate-y-0.5 active:translate-y-0.5 active:shadow-[inset_0px_3px_7px_#6903f6] skeu"
       >
-        Login
+        {isPending ? 
+        (<><svg aria-hidden="true" className="inline w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+          </svg></>) : 
+        <span>Login</span>}
       </button>
     </div>
   );
