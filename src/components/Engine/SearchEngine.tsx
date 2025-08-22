fac42280952e30a6e5fbe0541ef92428430912aa
AmirAbbasPZK0 <amirabbaspezeshki2@gmail.com>
2025-08-22 14:52:01 +0330

    D

--- src/components/Engine/SearchEngine.tsx
+++ src/components/Engine/SearchEngine.tsx
@@ -1,19 +1,16 @@
 "use client"
 
-import { usePathname } from "next/navigation";
 import PropmptYard from "../Engine/PromptYard";
 import { useAppSelector } from "@/services/redux/store";
 
 const SearchEngine = () => {
-
-    const pathname = usePathname()
     
     const isNew = useAppSelector(state => state.resourceSlice.isNew)
 
     const user = useAppSelector(state => state.userSlice)
 
     return (<>
-        <div className={`min-h-screen w-full flex flex-col items-center ${pathname.includes("c") ? "justify-start" : "justify-center"} dark:bg-[#111828]`}>
+        <div className={`min-h-screen w-full flex flex-col items-center justify-center dark:bg-[#111828]`}>
             {isNew && <div className="w-full flex items-center justify-center">
                 {user.isLogin ? 
                 <h3 className="text-[24px] md:text-[30px] md:font-light font-normal text-zinc-900 dark:text-slate-200 antialiased text-center">
